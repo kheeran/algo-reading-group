cf78101bb57498674503b0de9b1135692d14d811
Kheeran <kn16063@bristol.ac.uk>
2023-10-06 13:34:32 +0100

    fixing deploy?

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git pull
 git checkout dev
 commit_id=$(git rev-parse HEAD)
 bundle install
-bundle exec rake build
+bundle exec jekyll build
 echo "BUILD COMPLETE!"
 git checkout master
 rm -r !("_site")
